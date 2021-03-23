2cc2ba79300e05b5b26a9087e8c1f09bad8d9e00
luiz <luiz.lgvasconcelos@gmail.com>
2021-03-22 22:37:13 -0300

    ajustes nos testes do solid

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -1,3 +1,4 @@
+
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -6,11 +7,24 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
+
   constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+  execute({user_id}: IRequest): User[] {
+    
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("Mensagem do erro");
+    }
+
+    if (user.admin == false) {
+      throw new Error("User is not an admin!");
+    }
+
+    return this.usersRepository.list();
   }
+
 }
 
 export { ListAllUsersUseCase };
